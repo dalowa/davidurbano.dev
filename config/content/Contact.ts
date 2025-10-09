397b49dd57a3c293bc18734d12dac7254381cdc7
dalowa <davidurbano.dev@gmail.com>
2025-10-09 00:04:42 -0500

    feat(data): information for contact

---
+++ config/content/Contact.ts
@@ -0,0 +1,36 @@
+import { GithubIcon, InstagramIcon, LinkedinIcon } from "@/src/components/icons";
+import { SITE_CONFIG } from "./static-website-data";
+
+// config/contact.ts
+export const CONTACT_CONFIG = {
+  title: "Let's build something together",
+  subtitle: "I'm always open to discussing new projects, creative ideas, or opportunities to be part of your visions",
+  formTitle: "Send a message",
+  socialTitle: "Or Follow Me",
+  fields: [
+    { name: 'fullName', label: 'Full Name', type: 'text', placeholder: 'Your Name' },
+    { name: 'email', label: 'Email', type: 'email', placeholder: 'you@example.com' },
+    { name: 'subject', label: 'Subject', type: 'text', placeholder: 'Regarding...' },
+    { name: 'message', label: 'Message', type: 'textarea', placeholder: 'Your message...' }
+  ],
+  socialLinks: [
+    {
+      name: 'LinkedIn',
+      href: SITE_CONFIG.social.linkedin,
+      icon: LinkedinIcon,
+      title: 'David Urbano LinkedIn'
+    },
+    {
+      name: 'Instagram',
+      href: SITE_CONFIG.social.instagram,
+      icon: InstagramIcon,
+      title: 'David Urbano Instagram'
+    },
+    {
+      name: 'GitHub',
+      href: SITE_CONFIG.social.github,
+      icon: GithubIcon,
+      title: 'David Urbano GitHub'
+    }
+  ]
+} as const
--- config/content/index.ts
+++ config/content/index.ts
@@ -1,3 +1,4 @@
 export { homeHeroContent, homeAboutContent, homeProjectsContent } from './Home';
 export { PhotographerWebsite } from './Projects';
 export { NAVIGATION_ITEMS, FOOTER_LINKS, SITE_CONFIG } from './static-website-data';
+export { CONTACT_CONFIG } from './Contact';
