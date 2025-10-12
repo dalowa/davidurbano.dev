b3b5bfcabedf15e8bad178a335e04c34e9711e77
dalowa <davidurbano.dev@gmail.com>
2025-10-12 16:17:15 -0500

    feat: export useContactForm

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useIntersectionObserver } from './useIntersectionObserver';
 export { useDisappearLayoutEffect } from './useDisappearLayoutEffect';
 export { useCenterTag } from './useCenterTag';
+export { useContactForm } from './useContactForm';
