c0d49e3a5a2838257ed70a9c562dfa61e3bf028e
dalowa <davidurbano.dev@gmail.com>
2025-10-20 20:07:21 -0500

    fix: added width fit

--- src/components/ui/ToPageButton.tsx
+++ src/components/ui/ToPageButton.tsx
@@ -5,7 +5,7 @@ export const ToPageButton = ({redirectTo, children}: ToPageButtonProps) => {
   return (
       <Link
          href={redirectTo} 
-         className="inline-flex items-center gap-2 bg-red-dalowa hover:bg-red-dalowa/75 hover:scale-105 border border-transparent hover:border-red-dalowa px-6 py-3 rounded-lg font-semibold transition-all"
+         className="inline-flex w-fit items-center gap-2 bg-red-dalowa hover:bg-red-dalowa/75 hover:scale-105 border border-transparent hover:border-red-dalowa px-6 py-3 rounded-lg font-semibold transition-all"
       >
          {children}
          <span className="text-lg">→</span>
