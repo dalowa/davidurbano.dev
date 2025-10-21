7070985549ef778aacb9c67b2619119a72970b66
dalowa <davidurbano.dev@gmail.com>
2025-10-21 06:00:57 -0500

    new place holder for exps

--- src/components/features/home/HomeLab.tsx
+++ src/components/features/home/HomeLab.tsx
@@ -7,8 +7,10 @@ export const HomeLab = () => {
    return (
       <section 
          ref={elementRef}
-         className="mx-auto h-screen flex flex-col w-screen items-center text-white justify-center">
-         <h2 className="text-center font-extrabold text-3xl w-[50%] aspect-video bg-red-dalowa text-white mb-4">Banner coming soon...</h2>
+         className="mx-auto h-screen min-h-[600px] flex flex-col w-screen items-center text-white justify-center">
+         <div className="text-center flex justify-center items-center p-6 font-extrabold text-3xl w-[300px] lg:w-[55%] aspect-[9/16] lg:aspect-video bg-red-dalowa text-white mb-4">
+            <h2 className="mt-4">Gonna post some experiments soon...</h2>
+         </div>
       </section>
    )
 }
