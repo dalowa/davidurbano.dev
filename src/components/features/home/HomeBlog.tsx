5540a2a3f1ed56d00861c41cd0206b7adefa18b0
dalowa <davidurbano.dev@gmail.com>
2025-10-20 19:49:11 -0500

    fix: new sizes for images

--- src/components/features/home/HomeBlog.tsx
+++ src/components/features/home/HomeBlog.tsx
@@ -8,23 +8,23 @@ export const HomeBlog = () => {
          className="border-2d mx-auto mb-[10vh] border-red-dalowa aspect-[16/8] flex flex-col w-[75vw] max-w-6xl py-8 text-white justify-center">
          <h2 className="text-center font-extrabold text-3xl text-red-dalowa">Sharing My Process</h2>
          {/* Gallery Posts */}
-         <div className="py-7 flex flex-col gap-10 lg:flex-row lg:flex-wrap lg:justify-center">
+         <div className="py-7 flex flex-col gap-10 md:flex-row md:flex-wrap md:justify-center">
             {/* Post 1 */}
-            <div className="flex flex-col gap-3 lg:w-[325px]">
+            <div className="flex flex-col gap-3 md:w-[250px] lg:w-[350px]">
                <div className="bg-red-dalowa aspect-video rounded-lg"></div>
                <h4 className="text-2xl text-white-dalowa font-semibold">The Rise of Brutalism in Web Design</h4>
                <p className="font-light text-sm text-white-dalowa/85">{`Discover the raw and unfiltered approach of brutalism in web design, where functionality meets aesthetics in a bold statement.`}</p>
                <a className="text-red-dalowa/75" href="#">Read more</a>
             </div>
             {/* Post 2 */}
-            <div className="flex flex-col gap-3 lg:w-[325px]">
+            <div className="flex flex-col gap-3 md:w-[250px] lg:w-[350px]">
                <div className="bg-red-dalowa aspect-video rounded-lg"></div>
                <h4 className="text-2xl text-white-dalowa font-semibold">Why I Switched to TypeScript</h4>
                <p className="font-light text-sm text-white-dalowa/85">{`A quick look at the benefits and challenges of moving from JavaScript to TypeScript in modern web development.`}</p>
                <a className="text-red-dalowa/75" href="#">Read more</a>
             </div>
             {/* Post 3 */}
-            <div className="flex flex-col gap-3 lg:w-[325px]">
+            <div className="flex flex-col gap-3 md:w-[250px] lg:w-[350px]">
                <div className="bg-red-dalowa aspect-video rounded-lg"></div>
                <h4 className="text-2xl text-white-dalowa font-semibold">Building a Personal Brand Online</h4>
                <p className="font-light text-sm text-white-dalowa/85">{`Tips and strategies for creating a unique digital presence that stands out in today's crowded internet landscape.`}</p>
